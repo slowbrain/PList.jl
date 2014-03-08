d58a19a0c5e4659afd029948ac35580eb6fd4c66
Erik Engheim <erik.engheim@mac.com>
2014-03-09 00:00:53 +0100

    first version

--- README.md
+++ README.md
@@ -1,3 +1,31 @@
 # PList
 
+A module for reading and writing OS X plist in ASCII format. The binary and XML format is not supported presently.
+
+Example of plist ASCII format:
+
+	{
+	  Dogs = (
+	    {
+	      Name = "Scooby Doo";
+	      Age = 43;
+	      Colors = (Brown, Black);
+	    }
+	  );
+	}
+	
+    
+The plists can be read and written with `readplist` and `writeplist` which are designed to be similar to be similar to `readcsv` and `writecsv`. Example:
+
+    dict = readplist("example.plist")
+    writeplist("file.plist", dict)
+    
+The arrays and dictionaries used need to have elements of type `Any`. E.g. you can store an array defined as:
+
+    ["Brown","Black"]
+    
+Because that will infer its type as `Array{ASCIIString,1}` But you need the type to be `Array{Any,1}`. You accomplish this by defining the array like this:
+
+    {"Brown","Black"}
+    
 [![Build Status](https://travis-ci.org/ordovician/PList.jl.png)](https://travis-ci.org/ordovician/PList.jl)
--- src/PList.jl
+++ src/PList.jl
@@ -1,5 +1,110 @@
+#!/usr/bin/env julia
 module PList
 
-# package code goes here
+export  readplist,
+        readplist_string,
+        writeplist,
+        writeplist_string
+                
+using SimpleParser
+        
+encode(key::String, obj) = string(key, " = ", encode(obj), ";")
 
-end # module
+function encode(dict::Dict{Any, Any})
+    string("{", join([encode(key, value) for (key, value) in dict]), "}")
+end
+
+function encode(array::Vector{Any})
+    string( "(", join(map(encode, array), ", "), ")")
+end
+
+# Quotes strings which are not simple alphanumeric. 
+function encode(s::String)
+    if all(isalnum, s) && length(s) > 0 && isalpha(s[1])
+        s
+    else
+        "\"$s\""
+    end
+end
+
+encode(num::Real) = string(num)
+
+function encode(obj)
+    error("Can't encode $obj because it is of type $(typeof(obj))")
+end
+
+function parse_obj(parser::Parser)
+    token_type = parser.look_ahead.typ
+    lexeme = parser.look_ahead.lexeme
+    
+    if token_type == NUMBER
+        match(parser, NUMBER); integer(lexeme)
+    elseif token_type == STRING
+        match(parser, STRING); lexeme
+    elseif token_type == '('
+        parse_array(parser)
+    elseif token_type == '{'
+        parse_dict(parser)
+    end
+end
+
+
+function parse_array(parser::Parser)
+    array = cell(0)
+    
+    match(parser, '(')
+    if look_ahead_type(parser) == ')'
+        match(parser, ')')
+        return array
+    end
+    push!(array, parse_obj(parser))
+    while look_ahead_type(parser) != ')'
+        match(parser, ',')
+        push!(array, parse_obj(parser))
+    end
+    match(parser, ')')
+    array
+end
+
+function parse_dict(parser::Parser)
+    dict =  Dict{Any, Any}()
+    
+    match(parser, '{')
+    while look_ahead_type(parser) != '}'
+        key = parse_obj(parser)::String
+        match(parser, '=')
+        dict[key] = parse_obj(parser)
+        match(parser, ';')
+    end
+    match(parser, '}')
+    dict
+end
+
+function readplist_string(text::String)
+    lexer = Lexer(text)
+    parser = Parser(lexer)
+    parse_obj(parser)    
+end
+
+function readplist(stream::IO)
+    text = readall(stream)
+    readplist_string(text)
+end
+
+function readplist(filename::String)
+    open(readplist, filename)
+end
+
+writeplist_string(obj) = encode(obj)
+    
+function writeplist(stream::IO, obj)
+    print(stream, encode(obj))
+end    
+
+function writeplist(filename::String, obj)
+    open(filename, "w") do stream
+        writeplist(stream, obj)
+    end
+end
+
+end
