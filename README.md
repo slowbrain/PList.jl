199874e8a65f1f86232ace53fb012cbf92e64a4b
Erik Engheim <erik.engheim@mac.com>
2017-11-13 22:26:10 +0100

    Add initial code

--- README.md
+++ README.md
@@ -1,5 +1,25 @@
 # PList
 
+A module for reading and writing OS X plist in ASCII format. The binary and XML format is not supported presently.
+
+Example of plist ASCII format:
+
+	{
+	  Dogs = (
+	    {
+	      Name = "Scooby Doo";
+	      Age = 43;
+	      Colors = (Brown, Black);
+	    }
+	  );
+	}
+	
+    
+The plists can be read and written with `readplist` and `writeplist` which are designed to be similar to be similar to `readcsv` and `writecsv`. Example:
+
+    dict = readplist("example.plist")
+    writeplist("file.plist", dict)
+
 [![Build Status](https://travis-ci.org/ordovician/PList.jl.svg?branch=master)](https://travis-ci.org/ordovician/PList.jl)
 
 [![Coverage Status](https://coveralls.io/repos/ordovician/PList.jl/badge.svg?branch=master&service=github)](https://coveralls.io/github/ordovician/PList.jl?branch=master)
--- src/PList.jl
+++ src/PList.jl
@@ -1,5 +1,109 @@
 module PList
 
-# package code goes here
+export  readplist,
+        readplist_string,
+        writeplist,
+        writeplist_string
+                
+using SimpleParser
+       
+encode(key::String, obj) = string(key, " = ", encode(obj), ";")
 
-end # module
+function encode(dict::Dict{K, V}) where {K, V}
+    string("{", join([encode(key, value) for (key, value) in dict]), "}")
+end
+
+function encode(array::Vector{T}) where T
+    string( "(", join(map(encode, array), ", "), ")")
+end
+
+# Quotes strings which are not simple alphanumeric. 
+function encode(s::String)
+    if all(isalnum, s) && length(s) > 0 && isalpha(s[1])
+        s
+    else
+        "\"$s\""
+    end
+end
+
+encode(num::Real) = string(num)
+
+function encode(obj)
+    error("Can't encode $obj because it is of type $(typeof(obj))")
+end
+
+function parse_obj(parser::Parser)
+    token_type = parser.look_ahead.typ
+    lexeme = parser.look_ahead.lexeme
+    
+    if     NUMBER == token_type 
+        match(parser, NUMBER); parse(lexeme)
+    elseif STRING == token_type
+        match(parser, STRING); lexeme
+    elseif LPAREN == token_type
+        parse_array(parser)
+    elseif LBRACE == token_type
+        parse_dict(parser)
+    end
+end
+
+
+function parse_array(parser::Parser)
+    array = []
+    
+    match(parser, LPAREN)
+    if look_ahead_type(parser) == RPAREN
+        match(parser, RPAREN)
+        return array
+    end
+    push!(array, parse_obj(parser))
+    while look_ahead_type(parser) != RPAREN
+        match(parser, COMMA)
+        push!(array, parse_obj(parser))
+    end
+    match(parser, RPAREN)
+    array
+end
+
+function parse_dict(parser::Parser)
+    dict =  Dict{Any, Any}()
+    
+    match(parser, LBRACE)
+    while look_ahead_type(parser) != RBRACE
+        key = parse_obj(parser)::String
+        match(parser, EQUAL)
+        dict[key] = parse_obj(parser)
+        match(parser, SEMICOLON)
+    end
+    match(parser, RBRACE)
+    dict
+end
+
+function readplist_string(text::String)
+    lexer = Lexer(text)
+    parser = Parser(lexer)
+    parse_obj(parser)    
+end
+
+function readplist(stream::IO)
+    text = readstring(stream)
+    readplist_string(text)
+end
+
+function readplist(filename::String)
+    open(readplist, filename)
+end
+
+writeplist_string(obj) = encode(obj)
+    
+function writeplist(stream::IO, obj)
+    print(stream, encode(obj))
+end    
+
+function writeplist(filename::String, obj)
+    open(filename, "w") do stream
+        writeplist(stream, obj)
+    end
+end
+
+end
